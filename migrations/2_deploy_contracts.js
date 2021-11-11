bb28466b8aba3d02581cc0510da86740a6803dd6
saragarci <s@saragarci.com>
2021-11-11 09:28:36 +0100

    [IMP] improve comments in deploy_contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,11 +6,11 @@ module.exports = function(deployer) {
   deployer.deploy(Ticket);
   deployer.deploy(Poster);
   deployer.deploy(TicketBookingSystem, 
-    ["Star wars", "The Lord of the Rings"], // Show title
+    ["Star wars", "The Lord of the Rings"], // Title per show
     [3090233503328250, 3090233503328250], // Price per show
     [[1640628000, 1640800800], [1640714400, 1640887200]], // Available dates per show
     [[2, 3]], // Available rooms (rows, columns)
-    [[0, 0], [0, 0]], // room index per date
+    [[0, 0], [0, 0]], // Assignment of room (using index) per date
     "https://seatview.no/norwaycinema" // Link to gather seat views in every room
   );
 };
